36e272b9f6d36047a42a793c9bd64edbdf8b8436
Julien Roche <roche.jul@gmail.com>
2024-09-15 18:21:39 +0200

    feat: loader

--- resources/app.js
+++ resources/app.js
@@ -3,10 +3,19 @@ import { initializeServiceWorker } from './sw.js';
 
 await loadWebComponents();
 
+function removeLoader() {
+    const loaderElement = document.querySelector('.page__loader');
+    document.body.removeChild(loaderElement);
+
+    document.querySelector('.page').classList.remove('page--loading');
+}
+
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', async () => {
         await initializeServiceWorker();
+        removeLoader();
     });
 } else {
     await initializeServiceWorker();
+    removeLoader();
 }
