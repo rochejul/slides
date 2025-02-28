87642f1d661831b2be6adc12d2ad7e6ce463a17c
Julien Roche <roche.jul@gmail.com>
2025-02-28 21:09:44 +0100

    feat: create jro-figure

---
+++ resources/components/figure/figure.js
@@ -0,0 +1,36 @@
+class Figure extends HTMLElement {
+    #root;
+
+    constructor() {
+        super();
+
+        this.#root = this.attachShadow({ mode: 'closed' });
+    }
+
+    connectedCallback() {
+        const template = document.querySelector('template[id="figure"]');
+        this.#root.appendChild(template.content.cloneNode(true));
+        this.render();
+    }
+
+    attributeChangedCallback(e) {
+        this.render();
+    }
+
+    render() {
+        const figcaption = this.#root.querySelector('figcaption');
+        const img = this.#root.querySelector('img');
+
+        if (!this.isConnected || !figcaption || !img) {
+            return;
+        }
+
+        const label = this.getAttribute('label');
+
+        figcaption.textContent = label;
+        img.setAttribute('alt', label);
+        img.setAttribute('src', this.getAttribute('src'));
+    }
+}
+
+customElements.define('jro-figure', Figure);
