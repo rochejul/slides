9169dabf176a5d35a150a7ba1a05721230bfaa77
Julien Roche <roche.jul@gmail.com>
2026-01-04 15:20:29 +0100

    feat(snippet,css): center text

---
+++ snippets/css/center-text/app.js
@@ -0,0 +1,30 @@
+async function setupTextBox() {
+    const renderElements = document.querySelectorAll('.demo');
+    const codeElement = document.querySelector('code');
+    const selectElement = document.querySelector('select');
+
+    selectElement.addEventListener('input', (event) => {
+        event.preventDefault();
+        render();
+    });
+
+    function render() {
+        const currentValue = selectElement.value;
+        codeElement.textContent = currentValue;
+        renderElements.forEach((renderElement) => renderElement.setAttribute('style', currentValue));
+    }
+
+    render();
+}
+
+async function setup() {
+    await setupTextBox();
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', async () => {
+        await setup();
+    });
+} else {
+    await setup();
+}
