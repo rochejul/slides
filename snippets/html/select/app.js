6318b1e5bd33c2cd989d4bb3148bcbefaf110092
Roche julien <roche.jul@gmail.com>
2026-02-09 13:33:23 +0000

    feat(snippet,html): custom select

---
+++ snippets/html/select/app.js
@@ -0,0 +1,11 @@
+async function setup() {
+    hljs.initHighlightingOnLoad();
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', async () => {
+        await setup();
+    });
+} else {
+    await setup();
+}
